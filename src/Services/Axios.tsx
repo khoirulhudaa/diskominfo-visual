91902abf372f6b2327aabca7a31b8390ab0086da
valclassdevelop <developervalclass.com>
2024-03-13 12:29:19 +0700

    web user

--- src/Services/Axios.tsx
+++ src/Services/Axios.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:3600",
+    baseURL: "https://be-visual.vercel.app",
     headers: {
         "Content-Type": "application/json",
     },
